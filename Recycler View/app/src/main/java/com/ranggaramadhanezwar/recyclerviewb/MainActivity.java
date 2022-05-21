310fc8ac3840e9e504a08494eb4ae2bfdebb64af
ranggaezwar <101447935+ranggaezwar@users.noreply.github.com>
2022-05-21 11:17:06 +0700

    Add files via upload

---
+++ Recycler View/app/src/main/java/com/ranggaramadhanezwar/recyclerviewb/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ranggaramadhanezwar.recyclerviewb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
