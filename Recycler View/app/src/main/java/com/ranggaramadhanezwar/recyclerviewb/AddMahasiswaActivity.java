231d3e3d2e5770dde4d53481790f72f77803d357
ranggaezwar <ranggaramadhan887@gmail.com>
2022-05-28 13:02:47 +0700

    nah

--- Recycler View/app/src/main/java/com/ranggaramadhanezwar/recyclerviewb/AddMahasiswaActivity.java
+++ Recycler View/app/src/main/java/com/ranggaramadhanezwar/recyclerviewb/AddMahasiswaActivity.java
@@ -59,10 +59,10 @@ public class AddMahasiswaActivity extends AppCompatActivity {
                     e.printStackTrace();
                 }
 
-                String url = "https://stmikpontianak.net/01100862/tambahMahasiswa.php" +
+                String url = "https://stmikpontianak.net/011100862/tambahMahasiswa.php" +
                         "?nim=" + nim +
                         "&nama=" + nama +
-                        "jenisKelamin=" + jenisKelamin +
+                        "&jenisKelamin=" + jenisKelamin +
                         "&tempatLahir=" + tempatLahir +
                         "&tanggalLahir=" + tanggalLahir +
                         "&alamat=" + alamat +
--- Recycler View/app/src/main/java/com/ranggaramadhanezwar/recyclerviewb/MahasiswaAdapter.java
+++ Recycler View/app/src/main/java/com/ranggaramadhanezwar/recyclerviewb/MahasiswaAdapter.java
@@ -30,29 +30,21 @@ public class MahasiswaAdapter extends RecyclerView.Adapter<MahasiswaViewHolder>
     public void onBindViewHolder(@NonNull MahasiswaViewHolder holder, int position) {
         MahasiswaModel mm = _mahasiswaModelList.get(position);
         holder._jkImageView.setImageResource(R.drawable.boy);
-<<<<<<< HEAD
         if (mm.getJenisKelamin().toLowerCase().equals("perempuan"))
         {
             holder._jkImageView.setImageResource(R.drawable.girl);
         }
 
-=======
         if (mm.getJenisKelamin().toLowerCase().equals("perempuan")) {
             holder._jkImageView.setImageResource(R.drawable.girl);
         }
->>>>>>> 857314f9d086450393f9330bf21042d6db4f6d73
         holder._nimTextView.setText(mm.getNIM());
         holder._namaTextView.setText(mm.getNama());
         holder._jkTextView.setText(mm.getJenisKelamin());
 
         String jp = mm.getJP();
-<<<<<<< HEAD
         jp = jp.substring(0,2);
         holder._jkTextView.setText(jp);
-=======
-        jp = jp.substring(0, 2);
-        holder._jpTextView.setText(jp);
->>>>>>> 857314f9d086450393f9330bf21042d6db4f6d73
     }
 
     @Override
@@ -63,11 +55,6 @@ public class MahasiswaAdapter extends RecyclerView.Adapter<MahasiswaViewHolder>
         {
             count = _mahasiswaModelList.size();
         }
-<<<<<<< HEAD
-        return count;
-=======
         return  count;
-
->>>>>>> 857314f9d086450393f9330bf21042d6db4f6d73
     }
 }
